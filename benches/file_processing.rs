8ba6a9110261876fb3997d3dd63978cf1ad82a8d
ItsCbass <dietmilkstudios@gmail.com>
2025-09-27 22:04:52 -0400

    Trimmed up the file_processing.rs

--- benches/file_processing.rs
+++ benches/file_processing.rs
@@ -2,7 +2,6 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::fs;
 use std::path::PathBuf;
 use assert_fs::fixture::TempDir;
-use vexdoc::docgen::DocGenConfig;
 
 fn create_test_files(dir: &std::path::Path, count: usize) -> Vec<PathBuf> {
     let mut files = Vec::new();
@@ -30,15 +29,8 @@ fn another_function_{}() {{
 fn benchmark_file_processing(c: &mut Criterion) {
     let temp_dir = TempDir::new().unwrap();
     let test_files = create_test_files(temp_dir.path(), 100);
-    
-    let config = DocGenConfig {
-        inline_comments: "//".to_string(),
-        multi_comments: vec!["/*".to_string(), "*/".to_string()],
-        ignored_dirs: vec![],
-        file_extensions: vec!["rs".to_string()],
-    };
 
-    c.bench_function("process_files_sequential", |b| {
+    c.bench_function("file_io_sequential", |b| {
         b.iter(|| {
             let files = black_box(&test_files);
             let _result: Vec<_> = files
@@ -51,7 +43,7 @@ fn benchmark_file_processing(c: &mut Criterion) {
         })
     });
 
-    c.bench_function("process_files_parallel", |b| {
+    c.bench_function("file_io_parallel", |b| {
         b.iter(|| {
             use rayon::prelude::*;
             let files = black_box(&test_files);
